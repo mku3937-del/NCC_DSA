7f7a2f855c0ec59f827385a4c2d595eb0e02f9e2
mku3937-del <mku3937@gmail.com>
2025-12-06 11:27:19 +0300

    Add files via upload

---
+++ Millifærsla.py
@@ -0,0 +1,10 @@
+a=int(input())
+b=int(input())
+c=int(input())
+min_fee=min(a,b,c)
+if min_fee==a:
+    print("Monnei")
+elif min_fee==b:
+    print("Fjee")
+elif min_fee==c:
+    print("Dolladollabilljoll")        
