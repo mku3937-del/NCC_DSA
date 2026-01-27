7cfeda6c2bed1c4451f39685ff4d0579b14ad58f
mku3937-del <mku3937@gmail.com>
2026-01-27 16:32:12 +0300

    Add files via upload

---
+++ yes or YES.py
@@ -0,0 +1,7 @@
+k=int(input())
+for i in range (k):
+    s=input()
+    if s=='yes' or s=='Yes' or s=='yEs'or s=='yeS'or s=='YEs'or s=='yES'or s=='YeS'or s=='YES':
+        print('YES')
+    else:
+        print('NO')    
