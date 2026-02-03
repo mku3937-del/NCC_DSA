1fba18ef6bbd6de5e8d38ddbd913e4f392877dc2
mku3937-del <mku3937@gmail.com>
2026-02-03 14:41:11 +0300

    Update Tram.py

--- Tram.py
+++ Tram.py
@@ -1,12 +1,8 @@
 n=int(input())
-x=[]
-y=[]
-p=[]
-for i in range (n):
+l=0
+m=[]
+for i in range(n):
     a,b=map(int,input().split())
-    x.append(a)
-    y.append(b)
-for i in range(n-1):
-    k=(y[i]-x[i+1])+y[i+1]
-    p.append(k)
-print(max(p))   
+    l=l-a+b
+    m.append(l)
+print(max(m))    
